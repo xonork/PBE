96566df0c4fa28a6408a14b57b6ccd0c9f5fe0e6
xonork <43812413+xonork@users.noreply.github.com>
2020-09-29 13:18:49 +0200

    Update read.py

--- Puzzle1/read.py
+++ Puzzle1/read.py
@@ -8,12 +8,12 @@ class Rfid_PN532:
 		pn532 = Pn532_i2c()
 		pn532.SAMconfigure
 
-		card_data = pn532.read_mifare().get_data()
-		hex_data = card_data.hex()
-		hex_length = len(hex_data)
-		uid = hex_data[(hex_length-8):(hex_length)].upper()
-		return  uid
-
+		card_data = pn532.read_mifare().get_data() #Aquest mètode retorna en binari
+		hex_data = card_data.hex() #El que faig aquí és passar-ho a un string hexadecimal
+		hex_length = len(hex_data) 
+		uid = hex_data[(hex_length-8):(hex_length)].upper() #Em vaig donar compte que em retornava més valors dels que hauria, amb el nfc-poll vaig apuntar el uid
+		return  uid						#d'una de les targes, i vaig veure si aquesta seqüència es repetia en alguna part del que em retornava
+									#el mètode read_mifare().get_data(), i efectivament, la seqüència estava al final del string 
 
 
 if __name__ == "__main__":
