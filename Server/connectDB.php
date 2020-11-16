53520b8c90cac9355e9612a757a135e18e4ed5da
xonork <43812413+xonork@users.noreply.github.com>
2020-11-16 16:15:05 +0100

    Update connectDB.php

--- Server/connectDB.php
+++ Server/connectDB.php
@@ -1,33 +1,33 @@
 <?php
-	class db_connection{
+	class dbConnection{
 		//atributs de la classe
-		public $db_host;
-		public $db_name;
-		public $db_user;
-		public $db_password;
-		public $char_code;
+		public $dbHost;
+		public $dbName;
+		public $dbUser;
+		public $dbPassword;
+		public $charCode;
 		public $connection;
 		//constructor on es crea l'objecte pertinent a la connexio
-		function __construct($db_host, $db_name, $db_user, $db_password, $char_code){
-			$this->db_host = $db_host;
-			$this->db_name = $db_name;
-			$this->db_user = $db_user;
-			$this->db_password = $db_password;
-			$this->char_code = $char_code;
+		function __construct($dbHost, $dbName, $dbUser, $dbPassword, $charCode){
+			$this->dbHost = $dbHost;
+			$this->dbName = $dbName;
+			$this->dbUser = $dbUser;
+			$this->dbPassword = $dbPassword;
+			$this->charCode = $charCode;
 		}
 		//funció encarregada d'inciar la conexió amb la base de dades i retorna la connexio.
 		function connect(){
 			//Iniciem la conexió
-			$this->connection=mysqli_connect($this->db_host,$this->db_user,$this->db_password,$this->db_name);
+			$this->connection=mysqli_connect($this->dbHost,$this->dbUser,$this->dbPassword,$this->dbName);
 				//si hi ha error en la connexio s'avisa al usuari i surt de l'execucio
 			if(mysqli_connect_errno()){
 				echo "Error al connectar el servidor amb la base de dades.";
 				exit();
 			}
 			//verifica si existeix una db amb el nom especificat com a atribut i si no es el cas avisa al usuari
-			mysqli_select_db($this->connection, $this->db_name) or die ("No s'ha pogut trobar la base de dades.");
+			mysqli_select_db($this->connection, $this->dbName) or die ("No s'ha pogut trobar la base de dades.");
 			//Modifiquem la codificació de carácters que utilitzarem
-			mysqli_set_charset($this->connection, $this->char_code);
+			mysqli_set_charset($this->connection, $this->charCode);
 			return $this->connection;	
 		}
 		//funcio per tancar la connexio amb la db
